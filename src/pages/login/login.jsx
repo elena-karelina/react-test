feec4b23c286e628fc1cb2444ebac019acd7ab4b
Elena Karelina <karelinae03@mail.ru>
2024-03-24 13:49:51 +0700

    husky lint

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -6,7 +6,7 @@ import { NextButton } from '../../shared/ui/button';
 import '@style/style.css';
 
 export const Login = () => {
-  const name = d333s;
+  const name = d333ыs;
   const {
     showNextButton,
     showVerificationInput,
